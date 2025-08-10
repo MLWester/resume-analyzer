93b28b8704202032bf93b4599ca8c0c456592108
Mason Wester <masonwester21@gmail.com>
2025-08-10 17:48:33 -0500

    Added page title and header

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "TalentScope AI" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <h1>Track your Applications & Resume Ratings</h1>
+      <h2>Review your submissions and check AI-powered feedback</h2>
+    </section>
+
+  </main>;
 }
